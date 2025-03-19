7c65bca888de83f32154cd02bc65f099aa06f5ea
eiseleb47 <benjamin.eisele0101@gmail.com>
2025-03-19 19:13:13 +0100

    Added different responses

--- hello.py
+++ hello.py
@@ -3,8 +3,23 @@ def HelloWorld():
     return
  
 def HomePlanet():
-    planet = input("What is your home planet? Inster here: ")
-    print(planet + ", I heard the weather sucks there.")
+    planet = input("What is your home planet? Enter here: ").lower()
+    responses = {
+    "earth": "Good choice.",
+    "venus": "I heard the weather is nice there.",
+    "mars": "I heard they have cool machines over there.",
+    "jupiter": "I heard the weather sucks there.",
+    "mercury": "Do you have a good view of the sun there?",
+    "saturn": "Is it true that you can surf on the rings?",
+    "uranus": "Do they have Uranium there?",
+    "neptune": "Is it true that it has a sub-surface ocean?"
+    }
+    if planet in responses:
+        print(responses[planet])
+    elif planet == "pluto":
+        print("I don't know how to tell you this...")
+    else:
+        print("What? Where even is that?")
     return
 
 HelloWorld()
